8edb2318ea814fdae8ae58b3395e5b97c12b6044
Vu Tran Kien <vutrankien.vn@gmail.com>
2019-11-28 17:47:29 +0900

    Add download_lambda.py

---
+++ download_lambda.py
@@ -0,0 +1,18 @@
+##!python3.7
+import boto3
+import urllib.request
+
+#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html#Lambda.Client.get_function
+def download_function(fname):
+    lam = boto3.client('lambda')
+    response = lam.get_function(
+        FunctionName=fname
+        #,Qualifier=Nil
+    )
+    url = response['Code']['Location']
+    #https://stackabuse.com/download-files-with-python/
+    funcZip = fname + '.zip'
+    urllib.request.urlretrieve(url, funcZip)
+
+if __name__ == "__main__":
+    download_function('VuNG-login')
